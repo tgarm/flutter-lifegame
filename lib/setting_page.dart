d239cc07bde3482abad9a1ffad0d84b37a628d8c
ruoshui <zal@plotbridge.com>
2022-05-24 16:57:18 +0800

    Add the contents of the settings

--- lib/setting_page.dart
+++ lib/setting_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
 
 class SettingPage extends StatefulWidget {
   const SettingPage({Key? key}) : super(key: key);
@@ -8,20 +9,87 @@ class SettingPage extends StatefulWidget {
 }
 
 class _SettingPageState extends State<SettingPage> {
+  final List<String> patterns = ['Pattern1', 'Pattern2', 'Pattern3', 'Custom'];
+  String? selectedPattern;
+  final List<String> sizes = ['small','middle','big'];
+  String? selectedSize;
+
+  final descTextStyle = const TextStyle(
+    fontSize: 16,
+    color: Colors.white,
+    fontFamily: 'Roboto',
+  );
+
+  final descButton = BoxDecoration(
+    borderRadius: BorderRadius.circular(14),
+    border: Border.all(
+      color: Colors.black38,
+    ),
+    color: Colors.blue
+  );
+   
+  final descDropButton = BoxDecoration(
+    border: Border.all(
+      color: Colors.black38
+    ),
+    color: Colors.black45
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Setting', textAlign: TextAlign.center ),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () => {
-          },
-          child: const Text('select pattern'),
+        appBar: AppBar(
+          title: const Text('Setting', textAlign: TextAlign.center),
+          centerTitle: true,
         ),
-      )
-    );
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              DropdownButtonHideUnderline(
+                child: DropdownButton2(
+                  hint: const Text('select pattern'),
+                  items: patterns
+                    .map((item) => DropdownMenuItem<String>(
+                          value: item,
+                          child: Text(
+                            item,
+                            style: descTextStyle
+                          ),
+                        ))
+                    .toList(),
+                value: selectedPattern,
+                onChanged: (value) {
+                  setState(() {
+                    selectedPattern = value as String;
+                  });
+                },
+                buttonDecoration: descButton,
+                dropdownDecoration: descDropButton,
+              )),
+              DropdownButtonHideUnderline(
+                child: DropdownButton2(
+                hint: const Text('select size'),
+                items: sizes
+                    .map((item) => DropdownMenuItem<String>(
+                          value: item,
+                          child: Text(
+                            item,
+                            style: descTextStyle
+                          ),
+                        ))
+                    .toList(),
+                value: selectedSize,
+                onChanged: (value) {
+                  setState(() {
+                    selectedSize = value as String;
+                  });
+                },
+                buttonDecoration: descButton,
+                dropdownDecoration: descDropButton,
+              )),
+            ],
+          ),
+        ));
   }
 }
